839c1ca601fac48c81fecd381bb7747a6a6c49c5
shreyapandey2423 <shreyapandey2423@gmail.com>
2026-07-07 20:00:43 +0530

    Refactor Vite config for server and build options
    Updated server configuration and build settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    build: { 
+
+    build: {
       assetsInlineLimit: 0,
-      chunkSizeWarningLimit: 2000
+      chunkSizeWarningLimit: 2000,
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      host: '0.0.0.0',
+      port: 3000,
+      strictPort: true,
+      hmr: {
+        protocol: 'wss',
+        clientPort: 443,
+      },
     },
   };
 });
